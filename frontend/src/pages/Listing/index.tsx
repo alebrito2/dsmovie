f0b0ce2affb08ba8e8b0410a82dabbf29fa051ab
alebrito2 <alexsanderbrito123@gmail.com>
2022-01-11 00:59:26 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,34 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
-function Listing(){
+function Listing() {
 
-    return(
-        <Pagination />
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+        </>
+
     );
 }
 
